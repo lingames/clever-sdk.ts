bb7a11505f04d1dc500a6dc64078b8297e4f6e19
aster <192608617@qq.com>
2025-07-28 14:08:38 +0800

    🔧 Fix kuai-shou report

---
+++ projects/clever-sdk/src/models/AdvertiseStage.ts
@@ -0,0 +1,12 @@
+export enum AdvertiseStage {
+    // 曝光
+    Expose = 'expose',
+    // 点击
+    Click = 'click',
+    // 填充
+    Fill = 'fill',
+    // 完成
+    Complete = 'complete',
+    // 异常中断
+    Abnormal = 'abnormal',
+}
---
+++ projects/clever-sdk/src/models/ReportContext.ts
@@ -0,0 +1,6 @@
+export interface ReportContext {
+    channel_id?: string;
+    version_id?: string;
+    player_anonymous?: string;
+    player_id?: string;
+}
